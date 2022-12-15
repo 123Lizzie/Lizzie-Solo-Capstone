8834293aec56a098097603de38d10f9d5aacdd0e
123Lizzie <123lizziehunt@gmail.com>
2022-12-14 20:15:55 -0700

    second push

--- blazePage.ts
+++ blazePage.ts
@@ -1,11 +1,49 @@
 import {BasePage} from './basePage';
-import {By} from 'selenium-webdriver';
+import {By, until} from 'selenium-webdriver';
 
 export class Blaze extends BasePage {
-    //need selectors
+    homeBtn: By = By.xpath('//a[@class="nav-link"])[1]');
+    cartBtn: By = By.xpath('(//a[@class="nav-link"])[4]');
+    loginBtn: By = By.xpath('(//a[@class="nav-link"])[5]');
+    signUpBtn: By = By.xpath('(//a[@class="nav-link"])[8]');
+    logoutBtn: By = By.xpath('(//a[@class="nav-link"])[6]');
+    categoriesBtn: By = By.xpath('//a[@id="cat"]');
+    phonesBtn: By = By.xpath('//a[text()="Phones"]');
+    laptopsBtn: By = By.xpath('//a[text()="Laptops"]');
+    monitorsBtn: By = By.xpath('//a[text()="Monitors"]');
+    previousBtn: By = By.xpath('//button[@id="prev2"]');
+    nextBtn: By = By.xpath('//button[@id="next2"]');
+    signUpUserInput: By = By.xpath('(//input[@class="form-control"])[3]');
+    signUpPassInput: By = By.xpath('(//input[@class="form-control"])[4]');
+    signUpBtn1: By = By.xpath('//button[text()="Sign up"]');
+    loginUserInput: By = By.xpath('//input[@id="loginusername"]');
+    loginPassInput: By = By.xpath('//input[@id="loginpassword"]');
+    loginBtn1: By = By.xpath('//button[text()="Log in"]');
+    samsungPhone: By = By.xpath('//a[text()="Samsung galaxy s6"]');
+    addToCart: By = By.xpath('//a[@class="btn btn-success btn-lg"]');
+    deleteBtn: By = By.xpath('(//a[text()="Delete"])[1]');
+    placeOrderBtn: By = By.xpath('//button[text()="Place Order"]');
+    orderNameInput: By = By.xpath('//input[@id="name"]');
+    orderCountryInput: By = By.xpath('//input[@id="country"]');
+    orderCityInput: By = By.xpath('//input[@id="city"]');
+    orderCreditCartInput: By = By.xpath('//input[@id="card"]');
+    orderMonthInput: By = By.xpath('//input[@id="month"]');
+    orderYearInput: By = By.xpath('//input[@id="year"]');
+    purchaseBtn: By = By.xpath('//button[text()="Purchase"]');
+    okBtn: By = By.xpath('//button[text()="OK"]');
 
     constructor() {
         super({url: "https://www.demoblaze.com/index.html"});
     };
-    //methods - repeatable functionality
-}
+    async navigate() {
+        await this.driver.get(this.url);
+        await this.driver.wait(until.elementLocated(this.homeBtn));
+    };
+    async login() {
+        await this.driver.wait(until.elementLocated(this.loginBtn));
+        await this.click(this.loginBtn);
+        await this.setInput(this.loginUserInput, 'Test Name');
+        await this.setInput(this.loginPassInput, 'password');
+        await this.click(this.loginBtn1);
+    };
+};
--- blazeTests.test.ts
+++ blazeTests.test.ts
@@ -1,26 +1,85 @@
+import {Builder, Capabilities, WebDriver} from 'selenium-webdriver';
 import {Blaze} from './blazePage';
+const driver: WebDriver = new Builder().withCapabilities(Capabilities.chrome()).build();
 const blaze = new Blaze();
 
 describe('Lizzie Solo Capstone Blaze Store Project', () => {
-    test('Can sign up for a new account', async () => {
+    beforeEach(async () => {
+        await blaze.navigate();
+    });
+    afterAll(async () => {
+        await driver.quit();
+    });
 
+    test('Can sign up for a new account', async () => {
+        await blaze.click(blaze.signUpBtn);
+        await blaze.setInput(blaze.signUpUserInput, '1');
+        await blaze.setInput(blaze.signUpPassInput, '1');
+        await blaze.click(blaze.signUpBtn1);
     });
     test('Can login and logout', async () => {
-
+        await blaze.login();
+        await blaze.click(blaze.logoutBtn);
     });
     test('Cart functionality while logged in', async () => {
-
+        await blaze.login();
+        await blaze.click(blaze.samsungPhone);
+        await blaze.click(blaze.addToCart);
+        await blaze.click(blaze.addToCart);
+        await blaze.click(blaze.cartBtn);
+        await blaze.click(blaze.deleteBtn);
+        await blaze.click(blaze.deleteBtn);
     });
     test('Cart functionality while not logged in', async () => {
-
+        await blaze.click(blaze.samsungPhone);
+        await blaze.click(blaze.addToCart);
+        await blaze.click(blaze.addToCart);
+        await blaze.click(blaze.cartBtn);
+        await blaze.click(blaze.deleteBtn);
+        await blaze.click(blaze.deleteBtn);
     });
     test('Can checkout while logged in', async () => {
-
+        await blaze.login();
+        await blaze.click(blaze.samsungPhone);
+        await blaze.click(blaze.addToCart);
+        await blaze.click(blaze.cartBtn);
+        await blaze.click(blaze.placeOrderBtn);
+        await blaze.setInput(blaze.orderNameInput, 'Test Name');
+        await blaze.setInput(blaze.orderCountryInput, 'USA');
+        await blaze.setInput(blaze.orderCityInput, 'Washington');
+        await blaze.setInput(blaze.orderCreditCartInput, '1234 1234 1234 1234');
+        await blaze.setInput(blaze.orderMonthInput, '1');
+        await blaze.setInput(blaze.orderYearInput, '24');
+        await blaze.click(blaze.purchaseBtn);
+        await blaze.click(blaze.okBtn);
     });
     test('Can checkout while not logged in', async () => {
-
+        await blaze.click(blaze.samsungPhone);
+        await blaze.click(blaze.addToCart);
+        await blaze.click(blaze.cartBtn);
+        await blaze.click(blaze.placeOrderBtn);
+        await blaze.setInput(blaze.orderNameInput, 'Test Name');
+        await blaze.setInput(blaze.orderCountryInput, 'USA');
+        await blaze.setInput(blaze.orderCityInput, 'Washington');
+        await blaze.setInput(blaze.orderCreditCartInput, '1234 1234 1234 1234');
+        await blaze.setInput(blaze.orderMonthInput, '1');
+        await blaze.setInput(blaze.orderYearInput, '24');
+        await blaze.click(blaze.purchaseBtn);
+        await blaze.click(blaze.okBtn);
     });
     test('Can navigate categories, use previous/next, and home button', async () => {
-
+        await blaze.click(blaze.phonesBtn);
+        await blaze.click(blaze.nextBtn);
+        await blaze.click(blaze.previousBtn);
+        await blaze.click(blaze.laptopsBtn);
+        await blaze.click(blaze.nextBtn);
+        await blaze.click(blaze.previousBtn);
+        await blaze.click(blaze.monitorsBtn);
+        await blaze.click(blaze.nextBtn);
+        await blaze.click(blaze.previousBtn);
+        await blaze.click(blaze.categoriesBtn);
+        await blaze.click(blaze.nextBtn);
+        await blaze.click(blaze.previousBtn);
+        await blaze.click(blaze.homeBtn);
     });
 });
